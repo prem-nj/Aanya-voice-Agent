9eb2182024f755a088947d55723905b467f9b1a6
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-25 15:21:41 +0530

    Setup auth routes and update user model schema

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/connection.js"
+import authRouter from "./Routes/auth.route.js"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 
 const app = express()
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("api/auth",authRouter)
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running on ${process.env.PORT}`)
--- server/models/usermodel.js
+++ server/models/usermodel.js
@@ -1,98 +1,119 @@
 import mongoose from "mongoose";
 
-const PageSchema = new mongoose.Schema({
-  name: String,
-  path: String,
-  keyword: {
-    type: [string],
-    default:[],
+const PageSchema = new mongoose.Schema(
+  {
+    name: String,
+    path: String,
+    keyword: {
+      type: [String],
+      default: [],
+    },
   },
-},{_id:false})
+  { _id: false }
+);
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required:true
-  },
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
 
-  email:{
-    type: String,
-    required: true,
-    unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    assistantName: {
+      type: String,
+      default: "Anaya",
+    },
+
+    businessName: {
+      type: String,
+      default: "",
+    },
+
+    businessType: {
+      type: String,
+      default: "",
+    },
+
+    businessDescription: {
+      type: String,
+      default: "",
+    },
+
+    tone: {
+      type: String,
+      enum: ["friendly", "professional", "sales"],
+      default: "friendly",
+    },
+
+    theme: {
+      type: String,
+      enum: ["light", "dark", "glass", "neon"],
+      default: "dark",
+    },
+
+    enableVoice: {
+      type: Boolean,
+      default: true,
+    },
+
+    pages: {
+      type: [PageSchema],
+      default: [],
+    },
+
+    enableNavigation: {
+      type: Boolean,
+      default: true,
+    },
+
+    geminiApiKey: {
+      type: String,
+      default: "",
+    },
+
+    geminiStatus: {
+      type: String,
+      enum: ["active", "quota_exceed", "invalid"],
+      default: "active",
+    },
+
+    totalMessage: {
+      type: Number,
+      default: 0,
+    },
+
+    plan: {
+      type: String,
+      enum: ["free", "pro"],
+      default: "free",
+    },
+
+    requestLimit: {
+      type: Number,
+      default: 200,
+    },
+
+    proExpireAt: {
+      type: Date,
+      default: null,
+    },
+
+    isSetupComplete: {
+      type: Boolean,
+      default: false,
+    },
   },
-  assistantName: {
-    type: String,
-    default:"Anaya"
-  },
-  businessName: {
-    type: Stirng,
-    default:""
-  },
-  buisnessType: {
-    type: String,
-    default:""
-  },
-  buisnessDescription: {
-    type: String,
-    default:""
-  },
-  tone: {
-    type: String,
-    enum: ["friendly", "professional", "sales"],
-    default:"friendly"
-  },
-  theme:{
-    type: String,
-    enum: ["light", "dark", "glass", "neon"],
-    default:"dark"
-  },
-  enableVoice: {
-    type: Boolean,
-    default:true
-  },
-  pages: {
-    type: [PageSchema],
-    default:[]
-  },
-  enableNaigation: {
-    type: Boolean,
-    default:true
-  },
-  
-  geminApiKey: {
-    type: String,
-    default:""
-  },
-  geminiStatus: {
-    type: String,
-    enum: ["active", "quota_exceed", "invalid"],
-    default:"active"
-  },
-  totalmessage: {
-    type: Number,
-    default:0
-  },
-  plan: {
-    type: String,
-    enum: ["free", "pro"],
-    default:"free"
-  },
-  requestLimit: {
-    type: Number,
-    default:200,
+  {
+    timestamps: true,
   }
-  ,
-  proExpireAt: {
-    type: Date,
-    default:null
-  }
-  , issetupComplete: {
-    type: Boolean,
-    default:false
-}
-  
-}, { timestamps: true })
+);
 
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
-const User = mongoose.models("user", userSchema);
-export default User
+export default User;
