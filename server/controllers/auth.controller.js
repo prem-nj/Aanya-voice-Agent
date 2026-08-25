3b1cb24c6e34aa2d4f2307087a368883fcf0d58c
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-25 15:21:25 +0530

    Implement Google authentication and logout functionality

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,18 +1,58 @@
-import User from "../models/usermodel"
+import { genToken } from "../config/token.js";
+import User from "../models/usermodel.js";
 
-export default googleAuth = async (req, res) => {
+const googleAuth = async (req, res) => {
   try {
-    const { name, email } = req.body
-    if (!name || !eamil) {
-    return res.status(400).json({message:"Name and email are required"})
-    }
-    let user = await User.findOne({ email });
-    if (!user) {
-      user=await User.create(name,email)
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+      return res.status(400).json({
+        message: "Name and email are required",
+      });
     }
 
-    
-  } catch(error) {
-    
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({
+        name,
+        email,
+      });
+    }
+
+    const token = await genToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({
+      message: `Google auth error: ${error.message}`,
+    });
   }
-}
+};
+
+export default googleAuth;
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+    });
+
+    return res.status(200).json({
+      message: "Logout successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: `Logout auth error: ${error.message}`,
+    });
+  }
+};
