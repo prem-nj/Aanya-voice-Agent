9399d4fde4381d749d662d8ad1e68518cd6b8405
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-25 14:55:52 +0530

    Add JWT token generation utility

---
+++ server/config/token.js
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken"
+
+export const gettoken = async (userID) => {
+  try {
+  const token=jwt.sign({userID},process.env.JWT_SECRET,{expiresIn:"7d"})
+    return token
+  } catch (error) {
+    console.log(error)
+  }
+}
