05c8ae5ca5a5a3259adcb4bb4e820084c2d03f1f
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-26 13:27:36 +0530

    Rename userID to userId in token generation logic

--- server/config/token.js
+++ server/config/token.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken"
 
-export const genToken = async (userID) => {
+export const genToken = async (userId) => {
   try {
-  const token=jwt.sign({userID},process.env.JWT_SECRET,{expiresIn:"7d"})
+  const token=jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"7d"})
     return token
   } catch (error) {
     console.log(error)
