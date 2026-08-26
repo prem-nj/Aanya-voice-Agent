897463b7c1be282a81dde35851319040ee546b00
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-26 11:09:26 +0530

    Configure CORS and fix auth route path

--- server/index.js
+++ server/index.js
@@ -3,14 +3,20 @@ import dotenv from "dotenv"
 import connectDB from "./config/connection.js"
 import authRouter from "./Routes/auth.route.js"
 import cookieParser from "cookie-parser"
-
+import cors from "cors"
 dotenv.config()
 
 const app = express()
+app.use(cors({
+  origin: "http://localhost:5173",
+    credentials:true
+}))
+
+
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("api/auth",authRouter)
+app.use("/api/auth",authRouter)
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running on ${process.env.PORT}`)
