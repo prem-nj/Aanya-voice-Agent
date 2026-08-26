0918e5580c13f4f5f5c58ba8dea20a609e3bf622
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-26 13:27:44 +0530

    Change cookie sameSite attribute to lax

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -25,7 +25,7 @@ const googleAuth = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: false,
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -44,7 +44,7 @@ export const logout = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: false,
-      sameSite: "strict",
+      sameSite: "lax",
     });
 
     return res.status(200).json({
