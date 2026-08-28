740bccc121e5c11fbe41c0eff6efd5ececd5f4b3
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-28 11:55:42 +0530

    Add route for saving assistant

--- server/Routes/user.route.js
+++ server/Routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { isAuth } from "../Middleware/isAuth.js"
-import { getCurrentUSer } from "../controllers/user.controller.js"
+import { getCurrentUSer, saveAssistant } from "../controllers/user.controller.js"
 
 const userRouter = express.Router()
 
 userRouter.get('/current-user', isAuth, getCurrentUSer)
+userRouter.get("/save-assistant",isAuth,saveAssistant)
 export default userRouter
