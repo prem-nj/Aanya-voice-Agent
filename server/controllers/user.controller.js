3c526f4127816aec388c1dc1231eff36d588f8d5
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-28 11:54:03 +0530

    Add saveAssistant controller method

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -19,3 +19,46 @@ export const getCurrentUSer = async (req, res) => {
     });
   }
 };
+
+
+export const saveAssistant = async (req,res) => {
+    try {
+        const {
+        assistantName,
+        businessName,
+        businessType,
+        businessDescription,
+        tone,
+        theme,
+        geminiApiKey,
+        pages,
+        } = req.body
+
+        const user = await User.findById(req.userId)
+        if(!user){
+            return res.status(404).json({message:"Failed to get current user"})
+        }
+        user.assistantName = assistantName;
+        user.businessName = businessName;
+        user.businessType = businessType;
+        user.businessDescription = businessDescription;
+        user.tone = tone;
+        user.theme = theme;
+
+        if(geminiApiKey){
+            user.geminiApiKey = geminiApiKey;
+        }
+        user.geminiStatus = "active";
+        user.pages = pages || [];
+
+        user.isSetupComplete = true
+        await user.save()
+
+        return res.status(200).json({ message:
+          "Assistant saved successfully",
+        user})
+    } catch (error) {
+        return res.status(500).json({message:`failed to save Assistant ${error}`})
+    }
+}
+
