0ed29f1f1a2dbff3293b10f46788d0994b8554c9
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-29 16:08:33 +0530

    Register billing route in server index

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 import userRouter from "./Routes/user.route.js"
 import assistantRouter from "./Routes/assistant.route.js"
+import billingRouter from "./Routes/billing.route.js"
 dotenv.config()
 
 const app = express()
@@ -33,7 +34,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use("/api/user", privateCors,userRouter)
 app.use("/api/assistant",publicCors,assistantRouter)
-
+app.use("/api/billing",privateCors,billingRouter)
 app.use("/api/auth",privateCors,authRouter)
 
 app.listen(process.env.PORT, () => {
