40d18c482a9075a754826067ff5411bf2a1e4dde
PREM NAVNATH JADHAV <premjadhav21314@gmail.com>
2026-08-29 16:18:15 +0530

    Modify cookie options for authentication token
    Updated cookie settings for token to enhance security.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -23,9 +23,9 @@ const googleAuth = async (req, res) => {
     const token = await genToken(user._id);
 
     res.cookie("token", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      httpOnly: false,
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -42,9 +42,9 @@ export default googleAuth;
 export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
-      httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      httpOnly: false,
+      secure: true,
+      sameSite: "none",
     });
 
     return res.status(200).json({
