1bbd08dccd6d296239e3496bb56ce5088d57069d
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-29 10:47:46 +0530

    Update assistant config route path

--- server/Routes/assistant.route.js
+++ server/Routes/assistant.route.js
@@ -2,7 +2,7 @@ import express from "express"
 import { getAssistantConfig } from "../controllers/assistant.controller"
 const assistantRouter = express.Router()
 
-assistantRouter.get('/assistant/:userId', getAssistantConfig)
+assistantRouter.get('/config/:userId', getAssistantConfig)
 
 
 export default assistantRouter
